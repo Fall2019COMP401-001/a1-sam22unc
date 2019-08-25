2e3913a05a69af82c93bda2455f7a2cad61af96d
saman <saman@DESKTOP-GCVRTV9.wireless-1x.unc.edu>
2019-08-25 16:35:14 -0400

    This program will read in data that represents shopping orders from a number of customers and report the total amount spent on a per customer basis.

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,23 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int totalCustomers = scan.nextInt();
 		
+		for(int i = 0; i < totalCustomers; i++) {
+			String firstName = scan.next();
+			String lastName = scan.next();
+			int itemsBought = scan.nextInt();
+			
+			double total = 0.00;
+			for(int j = 0; j < itemsBought; j++) {
+				int itemQuantity = scan.nextInt();
+				String item = scan.next();
+				double price = scan.nextDouble();
+				
+				total += price * itemQuantity;
+			}
+			
+			System.out.printf("\n%s. %s: %.2f",firstName.substring(0,1), lastName, total);
+		}
 	}
 }
