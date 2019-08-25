cd749517ba857114287ea61011bf1dc1b0367a0f
saman <saman@DESKTOP-GCVRTV9.wireless-1x.unc.edu>
2019-08-25 16:36:37 -0400

    Added comments

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,13 +9,17 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		
+		//Sets number of customers
 		int totalCustomers = scan.nextInt();
 		
+		// Loop through data for every customer - name and unique items purchased
 		for(int i = 0; i < totalCustomers; i++) {
 			String firstName = scan.next();
 			String lastName = scan.next();
 			int itemsBought = scan.nextInt();
 			
+			// Calculate total from the quantity and price of items purchased
 			double total = 0.00;
 			for(int j = 0; j < itemsBought; j++) {
 				int itemQuantity = scan.nextInt();
@@ -25,6 +29,7 @@ public class A1Novice {
 				total += price * itemQuantity;
 			}
 			
+			// Print results
 			System.out.printf("\n%s. %s: %.2f",firstName.substring(0,1), lastName, total);
 		}
 	}
